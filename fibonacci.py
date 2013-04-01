cc0d84a2f24347c20ba27ef5c7890bcde11c21f5
Elaine Richards <elainerichards@gmail.com>
2013-03-31 19:46:18 -0700

    First version of fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+#
+# The fibonacci sequence 0, 1, 1, 2, 3 etc onto a bazillion.
+#
+# Since this can go on forever, let's stop at 10
+# 0 1 1 2 3 5 8 13 21 34
+
+a = 0
+b = 1
+c = 0
+
+print a,
+while ( c < 10):
+	print " ",
+	x = a  + b
+	a = b
+	b = x
+	print x,
+	c = c + 1
+print ""
+
+# How about recursively?
+
+def fib(a,b,x):
+	if x > 9:
+		return
+	c = a + b
+	print " "
+	print c,
+	a = b
+	b = c
+	x = x + 1
+	fib (a,b,x)
+
+fib (0,1,0)
